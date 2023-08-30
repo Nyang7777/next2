f4a96a52826115b6d424f36431fd745344265fb4
my <ehdvy2@naver.com>
2023-08-30 11:14:22 +0900

    first

---
+++ src/app/components/footer/Footer.js
@@ -0,0 +1,7 @@
+export default function Footer(){
+    return(
+        <div style={{width:'80%', margin:'auto', padding:'50px 0px', textAlign:'center', backgroundColor:'lightgray'}}>
+            Copyright &copy; 2023 한국ICT
+        </div>
+    )
+}
---
+++ src/app/page.js
@@ -0,0 +1,8 @@
+
+export default function Home() {
+  return (
+    <>
+      <h1 id="main">main창</h1>
+    </>
+  )
+}
